5d8e12673fa5d029c704529664cf237205509888
nishacodes <nishabatra27@gmail.com>
2014-02-10 14:52:25 -0500

    add routes for individual cat pages

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,13 +1,18 @@
 'use strict';
 
-var spacecatApp = angular.module('spacecatApp', []);
+var spacecatControllers = angular.module('spacecatControllers', []); // array would have any dependencies
 
-spacecatApp.controller('CatListCtrl', function($scope, $http){
-  
-  $http.get('cats/cats.json').success(function(data){
-    $scope.cats = data;
-  });
+spacecatApp.controller('CatListCtrl', ['$scope', '$http', 
+  function($scope, $http){
+    $http.get('cats/cats.json').success(function(data){
+      $scope.cats = data;
+    });
+    $scope.orderProp = 'name';
+}]);
 
-  $scope.orderProp = 'name';
-
-});
+spacecatApp.controller('CatDetailCtrl', ['$scope', '$http', '$routeParams',
+  function($scope, $http, $routeParams){
+    $http.get('cats/cats-' + $routeParams.catId + '.json').success(function(data) {
+      $scope.cat = data[0];
+    });
+}]);
